85230f8bd16ac3f09e6897190d91109d40e9ba8d
pedravil <pedravil@gmail.com>
2023-04-01 23:13:30 +0100

    Create FirebaseCloudMessagingServiceListener.java

---
+++ src/android/FirebaseCloudMessagingServiceListener.java
@@ -0,0 +1,28 @@
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        
+        // Check if message contains data payload.
+        if (remoteMessage.getData().size() > 0) {
+            // Get the notification data.
+            String title = remoteMessage.getData().get("title");
+            String body = remoteMessage.getData().get("body");
+            String imageUrl = remoteMessage.getData().get("image_url");
+
+            // Do something with the notification data.
+            // For example, show a notification to the user.
+            showNotification(title, body, imageUrl);
+        }
+    }
+
+    private void showNotification(String title, String body, String imageUrl) {
+        // Implement the code to show a notification to the user.
+        // You can use the NotificationCompat.Builder class to create a notification.
+        // You can also use an external library like Picasso to load the image from the URL.
+    }
+}
