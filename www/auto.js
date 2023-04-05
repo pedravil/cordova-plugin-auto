6c93651e3e90126f7359bc7846a078ee444b3dc6
pedravil <pedravil@gmail.com>
2023-04-05 21:42:26 +0100

    Update auto.js

--- www/auto.js
+++ www/auto.js
@@ -4,16 +4,16 @@ module.exports = {
     
     AndroidAutoSendNotification: function(title, message) {
         cordova.exec(null, null, 'AndroidAutoNotificationSender', 'sendNotification', [title, message]);
-    }/*,
+    }, /*
 	
 		AndroidAutoStartVoiceInteraction: function() {
         cordova.exec(null, null, 'AndroidAutoSpeechAnswerRetriever', 'startVoiceActivity', []);
-    },
+    },*/
 		
     AndroidAutoGetSpeechAnswer: function(successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, 'AndroidAutoSpeechAnswerRetriever', 'getSpeechAnswer', []);
-    },
-	
+    }
+/*,	
 		IsAndroidAutoConnected: function(successCallback, errorCallback) {
 				cordova.exec(successCallback, errorCallback, "AndroidAutoConnectionChecker", "isConnected", []);
 		};*/
