29ab36542a618d368d7cf1d15d525d134180df14
pedravil <pedravil@gmail.com>
2023-04-09 10:44:42 +0100

    Update AndroidAutoPlugin.java

--- src/android/AndroidAutoPlugin.java
+++ src/android/AndroidAutoPlugin.java
@@ -2,6 +2,13 @@ package com.androidauto.messaging;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -23,12 +30,51 @@ import java.util.Set;
 
 public class AndroidAutoPlugin extends CordovaPlugin {
   
+	Messenger androidAutoMessagingService = null;
+    boolean isBound;
+	
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+
+	}
+	
+  	private ServiceConnection serviceConnection = new ServiceConnection() {
+		
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			androidAutoMessagingService = new Messenger(service);
+			isBound = true;
+		}
+
+		public void onServiceDisconnected(ComponentName className) {
+			androidAutoMessagingService = null;
+			isBound = false;
+		}
+	}
   
-  
-  @Override
-  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-   
-    
-  }
+	private void sendNotificationAndroidAuto(String conversationId, String title, String description, CallbackContext callbackContext) throws JSONException {
+
+		Intent intent = new Intent(getApplicationContext(), AndroidAutoMessagingService.class);
+
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+		
+		if (!isBound) return;
+        
+        Message message = Message.obtain();
+        
+        Bundle bundle = new Bundle();
+        bundle.putString("MyString", "Message Received");
+        
+        message.setData(bundle);
+        
+        try {
+			
+        	androidAutoMessagingService.send(message);
+			
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+		
+	}
   
 }
