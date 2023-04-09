c8e7bc093707272fca7d8648956303ed7d9473fa
pedravil <pedravil@gmail.com>
2023-04-09 01:28:50 +0100

    Create AndroidAutoPlugin.java

---
+++ src/android/AndroidAutoPlugin.java
@@ -0,0 +1,34 @@
+package com.androidauto.messaging;
+
+import android.content.Context;
+import android.content.Intent;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+public class AndroidAutoPlugin extends CordovaPlugin {
+  
+  
+  
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+   
+    
+  }
+  
+}
