266752b8f236cc13430aa095168f685b14baf00c
pedravil <pedravil@gmail.com>
2023-04-09 01:30:30 +0100

    Update auto.js

--- www/auto.js
+++ www/auto.js
@@ -1,15 +1,5 @@
-/*global cordova, module*/
+var exec = require('cordova/exec');
 
-exports.AndroidAutoSendNotification = function(successCallback, errorCallback, title, message) {
-	cordova.exec(successCallback, errorCallback, 'AndroidAutoNotificationSender', 'sendNotification', [title, message]);
-};
-
-    
-exports.AndroidAutoGetSpeechAnswer = function(successCallback, errorCallback) {
-	cordova.exec(successCallback, errorCallback, 'AndroidAutoSpeechAnswerRetriever', 'getSpeechAnswer', []);
-};
-
-
-exports.IsAndroidAutoConnected =  function(successCallback, errorCallback) {
-	cordova.exec(successCallback, errorCallback, "AndroidAutoConnectionChecker", "isConnected", []);    
+exports.AndroidAutoSendNotification = function(title, message, successCallback, errorCallback) {
+	exec(successCallback, errorCallback, 'AndroidAutoNotificationSender', 'sendNotification', [title, message]);
 };
