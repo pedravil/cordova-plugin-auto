cd8dae171ea94ed8675801fd8c8d8bd8c7b9539c
pedravil <pedravil@gmail.com>
2023-04-12 01:22:57 +0100

    Update and rename AndroidAutoPlugin.js to androidAutoPlugin.js

--- www/androidAutoPlugin.js
+++ www/androidAutoPlugin.js
@@ -2,13 +2,13 @@ var exec = require('cordova/exec');
 
 var PLUGIN_NAME = 'AndroidAutoPlugin';
 
-/*exports.sendNotificationAndroidAuto = function(conversationId, title, message, successCallback, errorCallback) {
+exports.sendNotificationAndroidAuto = function(conversationId, title, message, successCallback, errorCallback) {
 	exec(successCallback, errorCallback, PLUGIN_NAME, 'sendNotificationAndroidAuto', [conversationId, title, message]);
 };
 
 exports.onWaitAnswerAndroidAuto = function(successCallback, errorCallback) {
 	exec(successCallback, errorCallback, PLUGIN_NAME, 'onWaitAnswerAndroidAuto', []);
-};*/
+};
 
 exports.onTest = function(successCallback, errorCallback) {
 	exec(successCallback, errorCallback, PLUGIN_NAME, 'onTest', []);
