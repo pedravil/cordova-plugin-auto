caac989104b2b5ab8db6e2bc909570c76e67bc0b
pedravil <pedravil@gmail.com>
2023-04-27 22:34:40 +0100

    Changes to be committed: modified:   src/android/AndroidAutoMessagingService.java

--- src/android/AndroidAutoMessagingService.java
+++ src/android/AndroidAutoMessagingService.java
@@ -4,7 +4,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.app.Notification.Action;
+import android.app.Notification;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
