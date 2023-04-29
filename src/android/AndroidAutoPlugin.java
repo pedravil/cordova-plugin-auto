7a7f74c4b7b49b9f27e1b0d62ad57a4e799cf313
pedravil <pedravil@gmail.com>
2023-04-29 15:49:44 +0100

    Changes to be committed: 	modified:   src/android/AndroidAutoPlugin.java

--- src/android/AndroidAutoPlugin.java
+++ src/android/AndroidAutoPlugin.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.ComponentName;
+import android.content.IntentFilter;
 
 import android.os.Bundle;
 import android.os.IBinder;
